e91ddcf6fd8493390bb53ac3963d5ce422c102a8
Lizzie <50921515+librasaur@users.noreply.github.com>
2019-06-20 12:48:23 +0100

    Started player movement
    Created a player movement script

---
+++ GameProject/Assets/Turn based assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Turn_based_assets.Scripts
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private Tilemap tilemap;
+        public Vector3 currentTile;
+
+
+        private void Start()
+        {
+
+        }
+
+
+        private void Update()
+        {
+            MovePlayer();
+            CheckNeighbours();
+        }
+
+
+        private void MovePlayer()
+        {
+            transform.position = currentTile;
+        }
+
+        private void CheckNeighbours()
+        {
+            
+        }
+    }
+}
