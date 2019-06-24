3f2164b56c6784256d9a9867f1f2a45501802eea
Ian Barrow <Barrowian1991@gmail.com>
2019-06-24 16:44:26 +0100

    Code review update 3

--- GameProject/Assets/TurnBasedAssets/Scripts/MouseSelection.cs
+++ GameProject/Assets/TurnBasedAssets/Scripts/MouseSelection.cs
@@ -5,35 +5,35 @@ namespace TurnBasedAssets.Scripts
     public class MouseSelection : MonoBehaviour
     {
         private ISelection _selection;
-        [SerializeField] public GameObject cube;
-        [SerializeField] public float distanceY;
-        [SerializeField] public float offset;
-        [SerializeField] public float gridSize;
-        [SerializeField] public Camera camera;
+        [SerializeField] private GameObject _cube;
+        [SerializeField] private float _distanceY;
+        [SerializeField] private float _offset;
+        [SerializeField] private float _gridSize;
+        [SerializeField] private Camera _camera;
         private Plane _plane;
         private Vector3 _distanceFromCamera;
         
 
         private void Start()
         {
-            var position = camera.transform.position;
-            _distanceFromCamera = new Vector3(position.x, position.y - distanceY,position.z);
+            var position = _camera.transform.position;
+            _distanceFromCamera = new Vector3(position.x, position.y - _distanceY,position.z);
             _plane = new Plane(Vector3.up, _distanceFromCamera);
         }
 
         private void Update()
         {
-            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
 
             if (_plane.Raycast(ray, out float gridSpace))
             {
                 Vector3 rawGridPoint = ray.GetPoint(gridSpace);
-                rawGridPoint -= Vector3.one * offset;
-                rawGridPoint /= gridSize;
+                rawGridPoint -= Vector3.one * _offset;
+                rawGridPoint /= _gridSize;
                 rawGridPoint = new Vector3(Mathf.Round(rawGridPoint.x), Mathf.Round(rawGridPoint.y), Mathf.Round(rawGridPoint.z));
-                rawGridPoint *= gridSize;
-                rawGridPoint += Vector3.one * offset;
-                if (cube != null) cube.transform.position = rawGridPoint;
+                rawGridPoint *= _gridSize;
+                rawGridPoint += Vector3.one * _offset;
+                if (_cube != null) _cube.transform.position = rawGridPoint;
             }
 
             if(Input.GetKeyDown(KeyCode.Mouse0))
