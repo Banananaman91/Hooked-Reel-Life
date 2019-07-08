0b83f6fce7be2a598add2e5ad3145c1b3d1ef7a2
Jinyaden <35196764+Jinyaden@users.noreply.github.com>
2019-07-08 11:22:41 +0100

    Action Combat shit
    Added in a few of the scripts I was working on

---
+++ GameProject/Assets/OpenWorld Assets/Scripts/trdprsnCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trdprsnCamera : MonoBehaviour
+{
+    [SerializeField] Transform lookAt;
+    [SerializeField] Transform camTransform;
+
+    private Camera cam;
+
+    private float distance = 15.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    //private float sensX = 4.0f;
+    //private float sensY = 1.0f;
+
+
+    private void Start()
+    {
+        camTransform = transform;
+        cam = Camera.main;
+    }
+    private void Update()
+    {
+        currentX += Input.GetAxis("Mouse X");
+        currentY -= Input.GetAxis("Mouse Y");
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        camTransform.position = lookAt.position + rotation * dir;
+        camTransform.LookAt(lookAt.position);
+    }
+}
---
+++ GameProject/Assets/OpenWorld Assets/Scripts/trdprsnCharacter.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trdprsnCharacter : MonoBehaviour
+{
+
+    [SerializeField] float movespeed;
+    [SerializeField] float rotatespeed;
+    private float rotX;
+    private float rotY;
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        Playerturn();
+    }
+
+    private void FixedUpdate()
+    {
+        Playermovement();
+    }
+
+    void Playermovement()
+    {
+        //forward and back controls
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey("w")) // if w and shift are pressed
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movespeed * 2.5f;
+        }
+        else if (Input.GetKey("w") && !Input.GetKey(KeyCode.LeftShift)) // if only w is pressed
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movespeed;
+        }
+        else if (Input.GetKey("s") && !Input.GetKey(KeyCode.LeftShift)) // if only s is pressed
+        {
+            transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movespeed;
+        }
+
+        //left and right controls
+        if (Input.GetKey("a") && !Input.GetKey("d")) // if only a is pressed
+        {
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movespeed;
+        }
+        else if (Input.GetKey("d") && !Input.GetKey("a")) // if only d is pressed
+        {
+            transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * movespeed;
+        }
+
+    }
+
+    void Playerturn()
+    {
+        //rotX += Input.GetAxis("Mouse Y") * Time.deltaTime * rotatespeed;
+        //rotY += Input.GetAxis("Mouse X") * Time.deltaTime * rotatespeed;
+
+        //transform.rotation = Quaternion.Euler(rotX, rotY, 0);
+        //cam.transform.rotation = Quaternion.Euler(rotX, rotY, 0);
+
+        transform.localEulerAngles = new Vector3(Camera.main.transform.localEulerAngles.x, Camera.main.transform.localEulerAngles.y, transform.localEulerAngles.z); // rotates the player to always face in the direction of the camera
+    }
+}
