62e80df39ed6ef2287471f0d363906fc911abb7e
Ian Barrow <Barrowian1991@gmail.com>
2019-07-24 16:01:43 +0100

    Created object positioner type
    This type repositions standard objects to whole values

---
+++ GameProject/Assets/TurnBasedAssets/Scripts/Controllers/ObjectPositioner.cs
@@ -0,0 +1,29 @@
+﻿using TurnBasedAssets.Scripts.Interface;
+using TurnBasedAssets.Scripts.MessageBroker;
+using TurnBasedAssets.Scripts.MouseController;
+using UnityEngine;
+
+namespace TurnBasedAssets.Scripts.Controllers
+{
+    public class ObjectPositioner : Controller
+    {
+        private IPosition _iPosition;
+        [SerializeField] private MouseSelection _mouseSelectionScript;
+
+        private void Start()
+        {
+            MessageBroker.MessageBroker.Instance.SendMessageOfType(new PositionControllerRequestMessage(this));
+            SetPosition();
+        }
+
+        private void SetPosition()
+        {
+            transform.position = _iPosition.RePosition(transform.position, _mouseSelectionScript.PlanePosition);
+        }
+
+        public override void Initialise(IPosition iPosition)
+        {
+            _iPosition = iPosition;
+        }
+    }
+}
